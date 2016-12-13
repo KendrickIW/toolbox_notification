fdd3118aae003a5ca5d8e3407716ea8739fefe75
Kendrick Williams <kendrick_williams1@homedepot.com>
2016-12-13 11:03:12 -0500

    Initial Commit

---
+++ background.js
@@ -0,0 +1,45 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+  Displays a notification with the current time. Requires "notifications"
+  permission in the manifest file (or calling
+  "Notification.requestPermission" beforehand).
+*/
+function show(message) {
+  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
+  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
+  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+  new Notification(hour + time[2] + ' ' + period, {
+    icon: 'icon_48.png',
+    body: message
+  });
+}
+
+// Conditionally initialize the options.
+if (!localStorage.isInitialized) {
+  localStorage.isActivated = true;   // The display activation.
+  localStorage.frequency = 1;        // The display frequency, in minutes.
+  localStorage.isInitialized = true; // The option initialization.
+}
+
+// Test for notification support.
+if (window.Notification) {
+  // While activated, show notifications at the display frequency.
+  if (JSON.parse(localStorage.isActivated)) { show(localStorage.message); }
+
+  var interval = 0; // The display interval, in minutes.
+
+  setInterval(function() {
+    interval++;
+
+    if (
+      JSON.parse(localStorage.isActivated) &&
+        localStorage.frequency <= interval
+    ) {
+      show(localStorage.message);
+      interval = 0;
+    }
+  }, 60000);
+}
